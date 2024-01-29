c6e2156c85293d6e015642b14cad1bd46785b184
Mathew Gacy <matt@mgacy.com>
2024-01-29 12:36:46 -0700

    Support `--version` (#28)
    * Write to output directory
    
    * Try without parent directory
    
    * Change Xcode plugin output
    
    * Add version file plugin and version
    
    * Add release workflows
    
    * Remove changes
    
    * Fix workflows
    
    * Revert change
    
    * Rename, use @main
    
    * Update name
    
    * Add release document
    
    * Update manifest for SPI
    
    * Use step output not environment

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         .plugin(name: "LingoPlugin", targets: ["LingoPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/mobelux/swift-version-file-plugin.git", from: "0.2.0")
     ],
     targets: [
         .executableTarget(
